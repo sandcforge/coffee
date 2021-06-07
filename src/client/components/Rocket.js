e70203e62fddf783d87e0a5bdc67a12c16050197
Sheng <sandcforge@gmail.com>
2021-06-06 21:07:06 -0700

    polish animation

--- src/client/components/Rocket.js
+++ src/client/components/Rocket.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
-
-
 export const Rocket = (props) => {
 
   const { message, active = true, onEnd } = props;
-  const breatheAnimation = keyframes`
-    0% { left: 0px; }
-    100% { left: 100%;}
- `
+  const windowWidth = window.innerWidth;
+  const slideIn = keyframes`
+    0% { left: -300px; }
+    100% { left: ${windowWidth-300}px;}`;
+
   const activeWrapper = styled.div`
     background-color: green;
     position: absolute;
-    height: 100px;
-    width: 100px;
-    animation-name: ${breatheAnimation};
-    animation-duration: 4s;
+    height: 50px;
+    width: 300px;
+    animation-name: ${slideIn};
+    animation-duration: 2s;
+    animation-timing-function: linear;
+    animation-fill-mode: forwards;
  `;
   const inactiveWrapper = styled.div`
     display: none;
@@ -24,8 +25,7 @@ export const Rocket = (props) => {
 
   const Wrapper = active ? activeWrapper : inactiveWrapper;
 
-
   return (<>
-    <Wrapper onAnimationEnd={onEnd}>{message}</Wrapper>
+    <Wrapper onAnimationEnd={onEnd}>{message + windowWidth}</Wrapper>
   </>);
 };
