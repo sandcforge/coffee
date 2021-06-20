291fdbcac68a6e20c0560dadd28c0762989faebd
Sheng <sandcforge@gmail.com>
2021-06-20 11:43:36 -0700

    cannot kick off two rockets back2back within 5 sec.

--- src/server/api.js
+++ src/server/api.js
@@ -1,27 +1,40 @@
+
 const miscRoutes = (app) => {
   app.get('/api/now', (req, res) => {
     res.json({ now: Date.now(), appName: 'COFFEE' });
   });
-
+  // Set a cooldown between kicking off the rocket.
+  let cooldown = false;
   app.post('/api/kickoff', async (req, res) => {
     const { message } = req.body;
-    try {
-      const rocket = req.app.get('rocket');
-      const idx = rocket.status.findIndex(i => i == -1);
-      if (idx === -1) {
-        res.sendStatus(404);
-        return;
-      }
-      rocket.status[idx] = 0;
-      rocket.message[idx] = message;
 
+    // Ignore the reqest if still cooling down.
+    if (cooldown) {
+      res.sendStatus(425);
+      return;
+    }
+
+    const rocket = req.app.get('rocket');
+    const idx = rocket.status.findIndex(i => i == -1);
+    if (idx === -1) {
+      res.sendStatus(404);
+      return;
+    }
+    rocket.status[idx] = 0;
+    rocket.message[idx] = message;
+    try {
       req.app.get('socketIoServer').emit('H2C', JSON.stringify(rocket));
+      cooldown = true;
+      setTimeout(() => {
+        cooldown = false;
+      }, 5000);
       res.json(rocket);
     }
     catch (e) {
       console.log(e);
       res.sendStatus(404);
     }
+
   });
 
   app.post('/api/requpdate', async (req, res) => {
