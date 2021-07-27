c9f669486f1d1924890916b9157684eca6ec5d94
Sheng <sandcforge@gmail.com>
2021-07-27 11:33:31 -0700

    add a fade in when switching the background image.

--- src/client/components/Background.js
+++ src/client/components/Background.js
@@ -1,22 +1,29 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import backgroundDay from '../../../public/backgroundDay.png';
 import backgroundNight from '../../../public/backgroundNight.png';
 import { useInterval } from '../utils.js';
 
-export const Background = () => {
+const Background_ = () => {
+  const fadeIn = keyframes`
+    0% {opacity:0;}
+    100% {opacity:1;}
+  `;
+
   const Wrapper = styled.img`
     position: fixed;
     width: 100%;
     height: 100%;
-    opacity: 0.5;
+    animation: ${fadeIn} 1s;
   `;
   const [backgroundImage, setBackgroundImage] = useState('day');
 
   useInterval(() => {
     setBackgroundImage(backgroundImage === 'day' ? 'night' : 'day');
-  }, 5000);
+  }, 10000);
 
   return (<Wrapper src={backgroundImage === 'day' ? backgroundDay : backgroundNight} />);
 };
 
+// use memo to avoid refreshing when the state is updated. Launching the rocket in our case.
+export const Background = React.memo(Background_);
